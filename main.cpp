1f74a13a12fba5e23ffbeebf9a87e49d4ae24611
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-11-26 18:04:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n, a=1, b=1,c, i;
+    cin >> n;
+        do{
+            cout <<a<< endl;
+            b=c;
+            c=a;
+            a=b+c;
+        }while(a<=n);
+    return 0;
 }
